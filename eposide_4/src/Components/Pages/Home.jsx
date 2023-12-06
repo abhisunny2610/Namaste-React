f46224277872110ff84e277c95456e6cbcbab6d1
Abhishek <abhisheksunny463@gmail.com>
2023-12-06 22:26:15 +0530

    Eposide 4

---
+++ eposide_4/src/Components/Pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../Utilities/Header'
+import Footer from '../Utilities/Footer'
+import HomeBody from '../Utilities/HomeBody'
+
+const Home = () => {
+  return <>
+    <Header />
+    <HomeBody />
+    <Footer />
+  </>
+}
+
+export default Home
