02bbc3557cfd4c878d67e9ab30060c835c53257a
Abhishek <abhisheksunny463@gmail.com>
2023-12-08 21:48:29 +0530

    Eposide 7

--- eposide_7/src/App.js
+++ eposide_7/src/App.js
@@ -1,12 +1,24 @@
-import Header from './Components/Utilities/Header';
 import Home from './Components/Pages/Home';
 import './App.css';
+import About from './Components/Pages/About';
+import { createBrowserRouter , RouterProvider} from 'react-router-dom';
+
+
+const appRouter = createBrowserRouter([
+  {
+    path: '/',
+    element : <Home />
+  },
+  {
+    path: '/about',
+    element: <About />
+  }
+])
 
 function App() {
   return (
     <div className="App">
-      {/* <Header /> */}
-      <Home />
+      <RouterProvider router={appRouter} />
     </div>
   );
 }
