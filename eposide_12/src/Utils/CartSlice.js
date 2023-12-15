3e65c77c14b50d2d9b11d7b576c9bac13f10d0db
Abhishek <abhisheksunny463@gmail.com>
2023-12-15 21:15:58 +0530

    Eposide 12 Redux

--- eposide_12/src/Utils/CartSlice.js
+++ eposide_12/src/Utils/CartSlice.js
@@ -8,8 +8,16 @@ const cartSlice = createSlice({
     reducers: {
         addItem: (state, action) => {
             state.items.push(action.payload);
+        },
+        clearCart: (state) => {
+            state.items = []
+        },
+        removeItem: (state, action) => {
+            state.items.pop();
         }
     }
 })
 
-export default cartSlice
+export const {addItem, removeItem, clearCart} = cartSlice.actions
+
+export default cartSlice.reducer
