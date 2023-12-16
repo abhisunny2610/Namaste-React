e333c2266f01c03b3c4258e5bd8ccb392b7e286c
Abhishek <abhisheksunny463@gmail.com>
2023-12-16 22:37:09 +0530

    Youtube Clone

--- youtube_clone/src/App.js
+++ youtube_clone/src/App.js
@@ -1,9 +1,43 @@
 import './App.css';
+import Header from './Components/Header'
+import Footer from './Components/Footer'
+import Error from './Pages/Error'
+import Watch from './Pages/Watch'
+import Home from './Pages/Home'
+import { Outlet, RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+const Applayout = () => {
+  return (
+    <>
+    <Header />
+    <Outlet />
+    <Footer />
+    </>
+  )
+}
+
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <Applayout />,
+    errorElement : <Error />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: "watch/:vId",
+        element: <Watch />
+      }
+    ]
+  }
+])
 
 function App() {
   return (
     <div className="App">
-   
+      <RouterProvider router={appRouter}/>
     </div>
   );
 }
