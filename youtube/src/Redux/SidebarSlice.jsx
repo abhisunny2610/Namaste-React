88e687fbae55a49094baf85b641927b5637e6280
Abhishek <abhisheksunny463@gmail.com>
2024-01-02 18:32:11 +0530

    MAking a youtube clone for porfolio project

---
+++ youtube/src/Redux/SidebarSlice.jsx
@@ -0,0 +1,12 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const SidebarSlice = createSlice({
+    name : 'sidebar',
+    initialState: {},
+    reducers:{
+
+    }
+})
+
+
+export default SidebarSlice.reducer
